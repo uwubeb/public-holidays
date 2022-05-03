70663b4c25d855ff908ef9205adfca5fdbb06b66
Arūnas Zamalis <8914148+uwubeb@users.noreply.github.com>
2022-05-03 17:24:07 +0300

    Added query parameter classes for input validation

---
+++ public-holidays/Data/Queries/CountryDateQueryParams.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace public_holidays.Data.Queries;
+
+public class CountryDateQueryParams
+{
+    [Required]
+    [StringLength(3)]
+    public string CountryCode { get; set; }
+    
+    [Required]
+    [Range(2011, 32767)] // range from public api
+    public string Year { get; set; }
+    [Required]
+    [Range(1, 12)]
+    public string Month { get; set; }
+    
+    [Required]
+    [Range(1, 31)] // different for different months but not going to handle that
+    public string Day { get; set; }
+}
---
+++ public-holidays/Data/Queries/CountryYearQueryParams.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace public_holidays.Data.Queries;
+
+public class CountryYearQueryParams
+{
+    [Required]
+    [StringLength(3)]
+    public string CountryCode { get; set; }
+    
+    [Required]
+    [Range(2011, 32767)] // range from public api
+    public int Year { get; set; }
+}
